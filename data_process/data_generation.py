0004c30bfde4238b988aa114894e6b74607d6607
artoman42 <70898630+artoman42@users.noreply.github.com>
2024-01-23 00:24:40 +0200

    changed data_processing for uploading IRIS dataset, updated gitignore and readme

--- data_process/data_generation.py
+++ data_process/data_generation.py
@@ -5,6 +5,7 @@ import logging
 import os
 import sys
 import json
+from sklearn import datasets
 
 # Create logger
 logger = logging.getLogger()
@@ -33,34 +34,21 @@ DATA_DIR = get_project_dir(conf['general']['data_dir'])
 TRAIN_PATH = os.path.join(DATA_DIR, conf['train']['table_name'])
 INFERENCE_PATH = os.path.join(DATA_DIR, conf['inference']['inp_table_name'])
 
-# Singleton class for generating XOR data set
+# Singleton class for achieving and splitting Iris Dataset
 @singleton
-class XorSetGenerator():
+class IrisDatasetGenerator():
     def __init__(self):
         self.df = None
-
-    # Method to create the XOR data
-    def create(self, len: int, save_path: os.path, is_labeled: bool = True):
-        logger.info("Creating XOR dataset...")
-        self.df = self._generate_features(len)
+    # Method to upload the Iris data
+    def upload(self, save_path: os.path, is_labeled: bool = True):
+        logger.info("Uploading Iris dataset...")
+        iris = datasets.load_iris(as_frame = True)
+        self.df = iris.data
         if is_labeled:
-            self.df = self._generate_target(self.df)
+            self.df['target'] = iris.target
         if save_path:
             self.save(self.df, save_path)
         return self.df
-
-    # Method to generate features
-    def _generate_features(self, n: int) -> pd.DataFrame:
-        logger.info("Generating features...")
-        x1 = np.random.choice([True, False], size=n)
-        x2 = np.random.choice([True, False], size=n)
-        return pd.DataFrame(list(zip(x1, x2)), columns=['x1', 'x2'])
-
-    # Method to generate target
-    def _generate_target(self, df: pd.DataFrame) -> pd.DataFrame:
-        logger.info("Generating target...")
-        df['y'] = np.logical_xor(df['x1'], df['x2'])
-        return df
     
     # Method to save data
     def save(self, df: pd.DataFrame, out_path: os.path):
@@ -71,7 +59,7 @@ class XorSetGenerator():
 if __name__ == "__main__":
     configure_logging()
     logger.info("Starting script...")
-    gen = XorSetGenerator()
-    gen.create(len=256, save_path=TRAIN_PATH)
-    gen.create(len=64, save_path=INFERENCE_PATH, is_labeled=False)
+    gen = IrisDatasetGenerator()
+    gen.upload(save_path=TRAIN_PATH)
+    gen.upload(save_path=INFERENCE_PATH, is_labeled=False)
     logger.info("Script completed successfully.")
