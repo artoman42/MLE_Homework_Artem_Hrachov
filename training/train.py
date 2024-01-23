e1d4e6fbe03e9d739524269bd374bab71619e0e1
artoman42 <70898630+artoman42@users.noreply.github.com>
2024-01-23 12:21:21 +0200

    training is changed to tensorflow DL model

--- training/train.py
+++ training/train.py
@@ -10,14 +10,16 @@ import json
 import logging
 import pandas as pd
 import time
+import tensorflow as tf
+from tensorflow.keras import layers, models
 from datetime import datetime
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 
 # Comment this lines if you have problems with MLFlow installation
-import mlflow
-mlflow.autolog()
+# import mlflow
+# mlflow.autolog()
 
 # Adds the root directory to system path
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -29,9 +31,9 @@ CONF_FILE = os.getenv('CONF_PATH')
 from utils import get_project_dir, configure_logging
 
 # Loads configuration settings from JSON
+CONF_FILE = "settings.json"
 with open(CONF_FILE, "r") as file:
     conf = json.load(file)
-
 # Defines paths
 DATA_DIR = get_project_dir(conf['general']['data_dir'])
 MODEL_DIR = get_project_dir(conf['general']['models_dir'])
@@ -72,14 +74,29 @@ class DataProcessor():
 
 
 class Training():
-    def __init__(self) -> None:
-        self.model = DecisionTreeClassifier(random_state=conf['general']['random_state'])
+    def __init__(self, input_dim, num_classes) -> None:
+        self.input_dim = input_dim
+        self.num_classes = num_classes
+        self.model = self.build_model()
+    
+    def build_model(self):
+        logging.info("Building model...")
+        model = models.Sequential()
+        model.add(layers.Dense(64, activation="relu", input_dim = self.input_dim))
+        model.add(layers.Dense(32, activation="relu"))
+        model.add(layers.Dense(self.num_classes, activation="softmax"))
+        return model
+    def compile_model(self, optimizer=conf['train']['optimizer'],
+                       loss=conf['train']['loss'], metrics=[conf['train']['metric']]):
+        self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
     def run_training(self, df: pd.DataFrame, out_path: str = None, test_size: float = 0.33) -> None:
+        logging.info("Compiling model...")
+        self.compile_model()
         logging.info("Running training...")
         X_train, X_test, y_train, y_test = self.data_split(df, test_size=test_size)
         start_time = time.time()
-        self.train(X_train, y_train)
+        self.train_model(X_train, y_train, X_test, y_test)
         end_time = time.time()
         logging.info(f"Training completed in {end_time - start_time} seconds.")
         self.test(X_test, y_test)
@@ -87,19 +104,28 @@ class Training():
 
     def data_split(self, df: pd.DataFrame, test_size: float = 0.33) -> tuple:
         logging.info("Splitting data into training and test sets...")
-        return train_test_split(df[['x1','x2']], df['y'], test_size=test_size, 
+        X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['target']), df['target'], test_size=test_size, 
                                 random_state=conf['general']['random_state'])
+        logging.info("Transforming target to one-hot...")
+        y_train_one_hot = tf.keras.utils.to_categorical(y_train, num_classes=self.num_classes)
+        y_test_one_hot =  tf.keras.utils.to_categorical(y_test, num_classes=self.num_classes)
+        return X_train, X_test, y_train_one_hot, y_test_one_hot
     
+    def train_model(self, X_train, y_train, X_val, y_val,
+                     epochs=conf["train"]["epochs"], batch_size=conf["train"]["batch_size"]):
+        history = self.model.fit(X_train, y_train, 
+                                 validation_data=(X_val, y_val),
+                                 epochs=epochs, batch_size=batch_size)
+        return history
     def train(self, X_train: pd.DataFrame, y_train: pd.DataFrame) -> None:
         logging.info("Training the model...")
         self.model.fit(X_train, y_train)
 
     def test(self, X_test: pd.DataFrame, y_test: pd.DataFrame) -> float:
         logging.info("Testing the model...")
-        y_pred = self.model.predict(X_test)
-        res = f1_score(y_test, y_pred)
-        logging.info(f"f1_score: {res}")
-        return res
+        loss, acc =self.model.evaluate(X_test, y_test) 
+        logging.info(f" Accuracy:{acc}, loss: {loss}")
+        return acc, loss
 
     def save(self, path: str) -> None:
         logging.info("Saving the model...")
@@ -107,19 +133,19 @@ class Training():
             os.makedirs(MODEL_DIR)
 
         if not path:
-            path = os.path.join(MODEL_DIR, datetime.now().strftime(conf['general']['datetime_format']) + '.pickle')
+            path = os.path.join(MODEL_DIR, datetime.now().strftime(conf['general']['datetime_format']) + '.keras')
         else:
             path = os.path.join(MODEL_DIR, path)
-
-        with open(path, 'wb') as f:
-            pickle.dump(self.model, f)
+        logging.info(f"{path}")
+        self.model.save(path)
+        
 
 
 def main():
     configure_logging()
-
     data_proc = DataProcessor()
-    tr = Training()
+    tr = Training(input_dim=conf['train']['input_dim'],
+                  num_classes=conf['train']['num_classes'])
 
     df = data_proc.prepare_data(max_rows=conf['train']['data_sample'])
     tr.run_training(df, test_size=conf['train']['test_size'])
